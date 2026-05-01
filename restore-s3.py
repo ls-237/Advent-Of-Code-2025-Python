09c2a02597c7defdc3e7ff9a4558fd1c78663fbd
ls-237 <45576849+ls-237@users.noreply.github.com>
2026-05-01 09:53:25 -0400

    Implement CSV inventory report for S3 objects
    Added functionality to create a CSV inventory report for S3 objects, including bucket name and object keys.

--- restore-s3.py
+++ restore-s3.py
@@ -1,3 +1,30 @@
-import botocore
+import csv
 from boto3 import client
-from datetime import datetime, timezone
+conn = client('s3')
+
+#The intention of this script is to create a CSV inventory report for a customer
+#This may be useful if a customer needs to run a batch op on a single prefix/bucket quickly
+# and cannot wait for an inventory to be generated
+#Optional prefix inventory
+
+
+#Put Bucket Here
+Bucket = ""
+Prefix = ""
+
+#Use Paginator ListV2 on specified bucket
+paginator = conn.get_paginator('list_objects_v2')
+pages = paginator.paginate(Bucket=Bucket, Prefix=Prefix, MaxKeys=1000)
+
+#Write to .Csv file named inventory.csv
+#Interate through pages
+#Write rows for Bucket name and object key to CSV file
+with open('inventory1.csv', 'w') as file:
+    for page in pages:
+        for object in page['Contents']:
+            #can add other columns here by adding for example to list storage class,
+            #add ",(object['StorageClass'])
+            key = [Bucket, (object['Key'])]
+            print(key)
+            csvwriter = csv.writer(file, delimiter=",")
+            csvwriter.writerow(key)
